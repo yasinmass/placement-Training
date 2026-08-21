be5cdaa9087f4b7f76a99702fa77d60b3fbe0bc8
Yasin Mass <yasinmass3069@gmail.com>
2026-08-21 08:59:49 +0530

    class 2

---
+++ count_words.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class count_words {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int count =0;
+        for (int i=0;i<str.length();i++){
+            char ch = str.charAt(i);
+            if(ch==' '){
+                count++;
+            }
+        }
+        count+=1;
+        System.out.println("count :"+ count);
+    }
+}
